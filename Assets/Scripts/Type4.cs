98ae2678a4dccc4f8bec8499fc5e338ebcb342d4
Ellen <eol50559@gmail.com>
2022-03-04 16:14:39 +0000

    Type 4 - look for health pack and gets it

--- Assets/Scripts/Type4.cs
+++ Assets/Scripts/Type4.cs
@@ -41,14 +41,56 @@ public class Type4 : MonoBehaviour
                 anim.SetTrigger("ThrowGrande");
             }
         }
-        // if(info.IsName("ThrowGrande")){
-
-        // }
+        if (info.IsName("LookForHealth"))
+        {
+            target = LookForTag("health");
+            if (target == null)
+            {
+                //no health packs
+                if (GetComponent<AmmoController>().getAmmo() > 80)
+                {
+                    anim.SetBool("ifHealthAbv", false);
+                }
+            }
+            print("looking for health");
+            GetComponent<NavMeshAgent>().SetDestination(target.transform.position);
+            if (Vector3.Distance(transform.position, target.transform.position) < 2)
+            {
+                GetComponent<NPCHealth>().setHealth(100);
+            }
+        }
 
     }
 
 
     public void playerEnteredAmbushArea(){
         anim.SetTrigger("goToAmbush");
+    }
+
+    GameObject LookForTag(string tag)
+    {
+        GameObject[] gs = GameObject.FindGameObjectsWithTag(tag);
+        if (gs.Length == 1)
+        {
+            return gs[0];
+        }
+        else if (gs.Length > 0)
+        {
+            GameObject returnValue = gs[0];
+            float distance = Vector3.Distance(transform.position, gs[0].transform.position);
+            foreach (GameObject j in gs)
+            {
+                if (Vector3.Distance(transform.position, j.transform.position) < distance)
+                {
+                    returnValue = j;
+                    distance = Vector3.Distance(transform.position, j.transform.position);
+                }
+            }
+            return returnValue;
+        }
+        else
+        {
+            return null;
+        }
     }
 }
